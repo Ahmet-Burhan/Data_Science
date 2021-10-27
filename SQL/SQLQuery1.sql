6e7ff9f3513cc4264222bbc2654d8ac3055dab18
Ahmet Burhan Boztash <boztasburhan@gmail.com>
2021-10-27 19:00:46 +0200

    asd

---
+++ SQL/SQLQuery1.sql
@@ -0,0 +1,20 @@
+SELECT A.product_id, A.product_name, B.product_id, B.order_id, C.order_id, C.customer_id, D.*
+FROM	product.product A, sale.order_item B, sale.orders C, sale.customer D
+WHERE	A.product_id = B.product_id
+AND		B.order_id = C.order_id
+AND		C.customer_id = D.customer_id
+AND		A.product_name = 'Trek Remedy 9.8 - 2019'
+
+
+SELECT DISTINCT state
+FROM sale.customer X
+WHERE	NOT EXISTS
+					(
+					SELECT A.product_id, A.product_name, B.product_id, B.order_id, C.order_id, C.customer_id, D.*
+					FROM	product.product A, sale.order_item B, sale.orders C, sale.customer D
+					WHERE	A.product_id = B.product_id
+					AND		B.order_id = C.order_id
+					AND		C.customer_id = D.customer_id
+					AND		A.product_name = 'Trek Remedy 9.8 - 2019'
+					AND		X.state = D.state
+					)
